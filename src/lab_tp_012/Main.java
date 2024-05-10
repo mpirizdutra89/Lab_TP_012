196baf6ba1cc17b81b69facd3e324faacf0e294e
FerrandoCarlos <cferrando2015@gmail.com>
2024-05-10 11:28:32 -0300

    Primer Commit

--- src/lab_tp_012/Main.java
+++ src/lab_tp_012/Main.java
@@ -4,6 +4,11 @@
  */
 package lab_tp_012;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Martin
@@ -14,7 +19,34 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Connection cn;
+        PreparedStatement ps;
+        int reg;
+        String driver, url, usr, pass, sql;
+        //
+        driver = "org.mariadb.jdbc.Driver";
+        url = "jdbc:mariadb://localhost:3306/lab1_tp10_bd";
+        usr = "root";
+        pass = "";
+
+        try {
+            //cargamos los drivers
+            Class.forName(driver);
+            //conexión a la BD
+            cn = DriverManager.getConnection(url, usr, pass);
+            //Agregar un empleado
+            sql = "INSERT INTO empleado(dni, apellido, nombre_empleado, acceso, estado) "
+                    + "VALUES (12345678,'Gomez','Nicolas',1,true),"
+                    + "(98765432,'Aguero','Fernando',2,true),"
+                    + "(55666774,'Marcone','Cintia',1,true);";
+            ps = cn.prepareStatement(sql);
+            reg = ps.executeUpdate();
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Error al cargar Driver" + ex.getMessage());
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al conectarse a la base de datos" + ex.getMessage());
+        }
+
     }
-    
+
 }
